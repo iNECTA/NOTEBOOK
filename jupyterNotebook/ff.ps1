8b427f6152df779a75bcb5d037e6caadde5d02b2
Fridrik Fridriksson <fridrik@inecta.com>
2023-06-15 10:14:23 -0400

    intial commit 2

--- jupyterNotebook/ff.ps1
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/iNECTA/NOTEBOOK
-
-
-docker build -t my-thebe-notebook .
-
-
-docker run -p 8887:8887 my-thebe-notebook
-
-
--- jupyterNotebook/start-notebook.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export JUPYTER_ALLOW_INSECURE_WRITES=1
-jupyter notebook --ip=0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.custom_display_url=http://${HOSTNAME}:8888
